47dc54f4b07ba594a2c03ddf40a3fb36fbf8f7c9
sonjavanteese <35209042+sonjavanteese@users.noreply.github.com>
2022-04-04 21:02:36 +0200

    update to vite

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
 import App from './App.svelte'
 
 const app = new App({
-  target: document.getElementById('app'),
-  hydrate: true,
+  target: document.getElementById('app')
 })
 
 export default app
